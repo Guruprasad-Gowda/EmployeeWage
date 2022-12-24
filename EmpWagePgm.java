d402531bc747004c2eedf06fa5ebe9925d0b25b6
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:42:47 +0530

    ADDED: Code to calculate Employee Monthly Wage

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -3,33 +3,42 @@ package com.employee_wage;
 public class EmpWagePgm {
 
     public static void main(String[] args) {
-        int WAGE_PER_HOUR = 20;
-        int DAY_HOUR = 8;
-        int PART_HOUR = 4;
+        final int WAGE_PER_HOUR = 20;
+        final int DAY_HOUR = 8;
+        final int PART_HOUR = 4;
+        final int WORKING_DAYS = 20;
+        int totalWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
-        double employeeCheck = Math.floor((Math.random() * 10) % 3);
+        for (int day = 1; day <= WORKING_DAYS; day++) {
+            System.out.println();
+            double employeeCheck = Math.floor((Math.random() * 10) % 3);
 
-        int dailyWage = 0;
-        switch ((int) employeeCheck) {
-            case 1:
-                System.out.println("Employee is Present");
-                dailyWage = WAGE_PER_HOUR * DAY_HOUR;
-                System.out.println("Daily Employee Wage : " + dailyWage);
-                System.out.println("Employee Worked for Full Time");
-                break;
+            int dailyWage = 0;
+            System.out.println("Day : " + day);
+            switch ((int) employeeCheck) {
+                case 1:
+                    System.out.println("Employee is Present");
+                    System.out.println("Employee Worked for Full Time");
+                    dailyWage = WAGE_PER_HOUR * DAY_HOUR;
+                    break;
 
-            case 2:
-                System.out.println("Employee is Present");
-                dailyWage = WAGE_PER_HOUR * PART_HOUR;
-                System.out.println("Daily Employee Wage : " + dailyWage);
-                System.out.println("Employee Worked for Part Time");
-                break;
+                case 2:
+                    System.out.println("Employee is Present");
+                    System.out.println("Employee Worked for Part Time");
+                    dailyWage = WAGE_PER_HOUR * PART_HOUR;
+                    break;
 
-            default:
-                System.out.println("Employee is Absent");
-                break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
+            System.out.println("Daily Employee Wage : " + dailyWage);
+            totalWage += dailyWage;
         }
 
+        System.out.println();
+        System.out.println("Total Wage of Employee : " + totalWage);
     }
+
 }
