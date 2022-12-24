350d178043d5ea3a5442d050b7802933ca0e889c
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:39:21 +0530

    ADDED: Code to calculate Employee using switch case

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -6,23 +6,29 @@ public class EmpWagePgm {
         int WAGE_PER_HOUR = 20;
         int DAY_HOUR = 8;
         int PART_HOUR = 4;
-        int dailyWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
         double employeeCheck = Math.floor((Math.random() * 10) % 3);
 
-        if (employeeCheck == 1) {
-            System.out.println("Employee is Present");
-            dailyWage = WAGE_PER_HOUR * DAY_HOUR;
-            System.out.println("Daily Employee Wage : " + dailyWage);
-            System.out.println("Employee Worked for Full Time");
-        } else if (employeeCheck == 2) {
-            System.out.println("Employee is Present");
-            dailyWage = WAGE_PER_HOUR * PART_HOUR;
-            System.out.println("Daily Employee Wage : " + dailyWage);
-            System.out.println("Employee Worked for Part Time");
-        } else {
-            System.out.println("Employee is Absent");
+        int dailyWage = 0;
+        switch ((int) employeeCheck) {
+            case 1:
+                System.out.println("Employee is Present");
+                dailyWage = WAGE_PER_HOUR * DAY_HOUR;
+                System.out.println("Daily Employee Wage : " + dailyWage);
+                System.out.println("Employee Worked for Full Time");
+                break;
+
+            case 2:
+                System.out.println("Employee is Present");
+                dailyWage = WAGE_PER_HOUR * PART_HOUR;
+                System.out.println("Daily Employee Wage : " + dailyWage);
+                System.out.println("Employee Worked for Part Time");
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
 
     }
