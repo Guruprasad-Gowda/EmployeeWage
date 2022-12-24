c1dab39c61ede6c5aa8513f28c937a5d44760a12
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:27:32 +0530

    ADDED: Code to calculate Part Time wage

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -5,6 +5,7 @@ public class EmpWagePgm {
     public static void main(String[] args) {
         int WAGE_PER_HOUR = 20;
         int DAY_HOUR = 8;
+        int PART_HOUR = 4;
         int dailyWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
@@ -14,7 +15,12 @@ public class EmpWagePgm {
             System.out.println("Employee is Present");
             dailyWage = WAGE_PER_HOUR * DAY_HOUR;
             System.out.println("Daily Employee Wage : " + dailyWage);
-
+            System.out.println("Employee Worked for Full Time");
+        } else if (employeeCheck == 2) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * PART_HOUR;
+            System.out.println("Daily Employee Wage : " + dailyWage);
+            System.out.println("Employee Worked for Part Time");
         } else {
             System.out.println("Employee is Absent");
         }
