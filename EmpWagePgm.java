5a7518f9976768836d7db4cb621eca20aa6230b2
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:49:28 +0530

    ADDED: Code to calculate Employee Total Wage till a condition satisfied

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -8,33 +8,44 @@ public class EmpWagePgm {
         final int PART_HOUR = 4;
         final int WORKING_DAYS = 20;
         int totalWage = 0;
+        int totalHour = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
-        for (int day = 1; day <= WORKING_DAYS; day++) {
+        for (int day = 1; day < 30; day++) {
             System.out.println();
             double employeeCheck = Math.floor((Math.random() * 10) % 3);
 
             int dailyWage = 0;
+            int dailyHour = 0;
             System.out.println("Day : " + day);
             switch ((int) employeeCheck) {
                 case 1:
                     System.out.println("Employee is Present");
                     System.out.println("Employee Worked for Full Time");
                     dailyWage = WAGE_PER_HOUR * DAY_HOUR;
+                    dailyHour = DAY_HOUR;
                     break;
 
                 case 2:
                     System.out.println("Employee is Present");
                     System.out.println("Employee Worked for Part Time");
                     dailyWage = WAGE_PER_HOUR * PART_HOUR;
+                    dailyHour = PART_HOUR;
                     break;
 
                 default:
                     System.out.println("Employee is Absent");
                     break;
             }
+
             System.out.println("Daily Employee Wage : " + dailyWage);
             totalWage += dailyWage;
+            totalHour += dailyHour;
+
+            if (day >= WORKING_DAYS) {
+                break;
+            }
+
         }
 
         System.out.println();
