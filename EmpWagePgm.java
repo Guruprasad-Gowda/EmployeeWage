cd96384667b365b9a1b38a13ad674ef62a55825c
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:21:34 +0530

    ADDED: Code to calculate daily wage

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -3,12 +3,18 @@ package com.employee_wage;
 public class EmpWagePgm {
 
     public static void main(String[] args) {
+        int WAGE_PER_HOUR = 20;
+        int DAY_HOUR = 8;
+        int dailyWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
         double employeeCheck = Math.floor((Math.random() * 10) % 3);
 
         if (employeeCheck == 1) {
             System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * DAY_HOUR;
+            System.out.println("Daily Employee Wage : " + dailyWage);
+
         } else {
             System.out.println("Employee is Absent");
         }
