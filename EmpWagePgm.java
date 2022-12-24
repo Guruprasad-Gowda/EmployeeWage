3a4a6dbbe92a8d2cae9e9732c2d996c8961783e7
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 07:10:14 +0530

    ADDED: code to display welcome message

---
+++ EmpWagePgm.java
@@ -0,0 +1,9 @@
+package com.employee_wage;
+
+public class EmpWagePgm {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+    
+}
