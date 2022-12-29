0e289752411d2bf858b36ecbfd89bc46aedf661b
NamaskaraGuru <guru8953@gmail.com>
2022-12-29 18:01:04 +0530

    ADDED: code using Class Method to Compute Employee Wage

--- EmpWagePgm.java
+++ EmpWagePgm.java
@@ -1,15 +1,27 @@
 package com.employee_wage;
 
 public class EmpWagePgm {
-
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    public static final int MAX_HRS_IN_MONTH = 10;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    private int EMP_RATE_PER_HOUR;
+    private int NUM_OF_WORKING_DAYS;
+    private int MAX_HRS_IN_MONTH;
 
     public static void main(String[] args) {
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
+        EmpWagePgm empWageBuilderOOPS = new EmpWagePgm();
+        empWageBuilderOOPS.computeWage(20, 20, 100);
+    }
+
+    public void computeWage(int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
+        this.EMP_RATE_PER_HOUR = empRatePerHour;
+        this.NUM_OF_WORKING_DAYS = numberOfWorkingDays;
+        this.MAX_HRS_IN_MONTH = maxHrs;
+        int empHrs;
+        int empWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+        int totalEmpWage = 0;
 
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
@@ -26,11 +38,13 @@ public class EmpWagePgm {
             }
 
             totalEmpHrs += empHrs;
-            System.out.println("Day#:" + totalWorkingDays + "Emp hr:" + empHrs);
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            System.out.println("Day# " + totalWorkingDays + " Emp Hrs: " + empHrs + " Emp Wage: " + empWage);
+            totalEmpWage += empWage;
         }
 
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
     }
-
+    
 }
