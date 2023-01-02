c05060d5286028ae18395a69bc68dfff7a481fc0
NamaskaraGuru <guru8953@gmail.com>
2023-01-02 06:49:32 +0530

    ADDED: code to Calculate employee wage for multiple companies

--- EmpWageBuilderOOPS.java
+++ EmpWageBuilderOOPS.java
@@ -1,6 +1,6 @@
 package com.employee_wage;
 
-public class EmpWagePgm {
+public class EmpWageBuilderOOPS {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     private int EMP_RATE_PER_HOUR;
@@ -9,11 +9,12 @@ public class EmpWagePgm {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage builder Using OOPS concepts");
-        EmpWagePgm empWageBuilderOOPS = new EmpWagePgm();
-        empWageBuilderOOPS.computeWage(20, 20, 100);
+        EmpWageBuilderOOPS empWageBuilderOOPS = new EmpWageBuilderOOPS();
+        empWageBuilderOOPS.computeWage("AMAZON", 20, 20, 100);
+        empWageBuilderOOPS.computeWage("FLIPCART", 10, 15, 100);
     }
 
-    public void computeWage(int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
+    public void computeWage(String company, int empRatePerHour, int numberOfWorkingDays, int maxHrs) {
         this.EMP_RATE_PER_HOUR = empRatePerHour;
         this.NUM_OF_WORKING_DAYS = numberOfWorkingDays;
         this.MAX_HRS_IN_MONTH = maxHrs;
@@ -22,7 +23,6 @@ public class EmpWagePgm {
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
         int totalEmpWage = 0;
-
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -36,15 +36,12 @@ public class EmpWagePgm {
                 default:
                     empHrs = 0;
             }
-
             totalEmpHrs += empHrs;
             empWage = empHrs * EMP_RATE_PER_HOUR;
             System.out.println("Day# " + totalWorkingDays + " Emp Hrs: " + empHrs + " Emp Wage: " + empWage);
             totalEmpWage += empWage;
         }
-
         totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: " + totalEmpWage);
+        System.out.println("Total Emp Wage of an employee in " + company + " is " + totalEmpWage);
     }
-    
 }
